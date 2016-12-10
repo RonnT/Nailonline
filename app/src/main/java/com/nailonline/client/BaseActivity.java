871fc3632fff2d04ba3cbe27e8c9048b5e59fa6e
Roman Titov <ron74@bk.ru>
2016-12-10 17:35:15 +0500

    wokring with main menu

---
+++ app/src/main/java/com/nailonline/client/BaseActivity.java
@@ -0,0 +1,12 @@
+package com.nailonline.client;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Roman T. on 10.12.2016.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+
+    //to set themes
+}
---
+++ app/src/main/java/com/nailonline/client/MainActivity.java
@@ -0,0 +1,31 @@
+package com.nailonline.client;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onMasterClick(View v){
+        Toast.makeText(this,"onMasterClick", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onServiceClick(View v){
+        Toast.makeText(this,"onServiceClick", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onGiftClick(View v){
+        Toast.makeText(this,"onGiftClick", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onProfileClick(View v){
+        Toast.makeText(this,"onProfileClick", Toast.LENGTH_SHORT).show();
+    }
+
+}
